3f2cbe20ed2dff158c195abfcd5152c16484b4d0
wwhai <751957846@qq.com>
2020-04-01 15:11:18 +0800

    改了半天首页，算是完成了

--- docs/.vuepress/components/HomePageOne.vue
+++ docs/.vuepress/components/HomePageOne.vue
@@ -5,17 +5,20 @@
         v-if="$frontmatter.heroImage"
         :style="heroImageStyle || {}"
         :src="$withBase($frontmatter.heroImage)"
-        alt="hero">
+        alt="hero"
+      />
       <h1>{{ $frontmatter.heroText || $title }}</h1>
       <p class="description">{{ $description }}</p>
 
       <router-link class="btn-about" :to="$frontmatter.actionLink">{{ $frontmatter.actionText }}</router-link>
-      <img class="banner" src="./images/blog.svg" alt="">
+      <!-- <img class="banner" src="./images/blog.svg" alt /> -->
+      <img class="banner" src="./images/1.png" alt />
+      <hr>
     </section>
-    <section class="wish yesterday">
+    <section class="wish">
       <div class="wish-inner">
         <div class="img-wrapper">
-          <img src="./images/yesterday.svg" alt="">
+          <img src="./images/2.png" alt />
         </div>
         <div class="text-wrapper">
           <h1>{{ features[0].title }}</h1>
@@ -23,21 +26,21 @@
         </div>
       </div>
     </section>
-    <section class="wish today">
+    <section class="wish area">
       <div class="wish-inner">
         <div class="text-wrapper">
           <h1>{{ features[1].title }}</h1>
           <p class="description">{{ features[1].details }}</p>
         </div>
         <div class="img-wrapper">
-          <img src="./images/today.svg" alt="">
+          <img src="./images/3.png" alt />
         </div>
       </div>
     </section>
-    <section class="wish tomorrow">
+    <section class="wish">
       <div class="wish-inner">
         <div class="img-wrapper">
-          <img src="./images/tomorrow.svg" alt="">
+          <img src="./images/4.png" alt />
         </div>
         <div class="text-wrapper">
           <h1>{{ features[2].title }}</h1>
@@ -46,111 +49,126 @@
       </div>
     </section>
     <section class="md-content-wrapper">
-      <Content/>
+      <Content />
     </section>
   </article>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       downloads: 0
-    }
+    };
   },
 
   computed: {
-    features () {
-      return this.$frontmatter.features
+    features() {
+      return this.$frontmatter.features;
     },
-    heroImageStyle () {
-      return this.$frontmatter.heroImageStyle || {
-        maxHeight: '200px',
-        margin: '6rem auto 1.5rem'
-      }
+    heroImageStyle() {
+      return (
+        this.$frontmatter.heroImageStyle || {
+          maxHeight: "400px",
+          margin: "6rem auto 1.5rem"
+        }
+      );
     }
   },
 
-  created () {
-    const date = new Date()
-    const year = date.getFullYear()
-    const mounth = date.getMonth() + 1
-    const day = date.getDate()
-
+  created() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const mounth = date.getMonth() + 1;
+    const day = date.getDate();
   },
 
   methods: {
-    toThousandslsFilter (num) {
-      const numStr = String(num)
-      if (numStr === '' || numStr == undefined) {
-        return ''
+    toThousandslsFilter(num) {
+      const numStr = String(num);
+      if (numStr === "" || numStr == undefined) {
+        return "";
       }
-      const IntPart = /\./g.test(numStr) ? numStr.slice(0, numStr.indexOf('.')) : numStr
-      const FloatPart = /\./g.test(numStr) ? numStr.substring(numStr.indexOf('.')) : ''
+      const IntPart = /\./g.test(numStr)
+        ? numStr.slice(0, numStr.indexOf("."))
+        : numStr;
+      const FloatPart = /\./g.test(numStr)
+        ? numStr.substring(numStr.indexOf("."))
+        : "";
 
-      const orderPrice2 = (+IntPart || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ',')) + FloatPart
-      return orderPrice2
+      const orderPrice2 =
+        (+IntPart || 0)
+          .toString()
+          .replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ",")) +
+        FloatPart;
+      return orderPrice2;
     },
 
-    _fetch (packages, dateRange) {
-      const BASE_URI = 'https://api.npmjs.org/downloads/point/'
-      return new Promise ((resolve, reject) => {
-        fetch(`${BASE_URI}${dateRange}/${packages}`).then(this._parseJSON).then(res => {
-          resolve(res)
-        }).catch(err => {
-          reject(err)
-        })
-      })
+    _fetch(packages, dateRange) {
+      const BASE_URI = "https://api.npmjs.org/downloads/point/";
+      return new Promise((resolve, reject) => {
+        fetch(`${BASE_URI}${dateRange}/${packages}`)
+          .then(this._parseJSON)
+          .then(res => {
+            resolve(res);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
     },
 
-    _parseJSON (response) {
-      return response.json()
+    _parseJSON(response) {
+      return response.json();
     },
 
-    _handleDateRange (dateRange) {
-      const index = dateRange.indexOf(':')
-      if (index> -1) {
-        const dr = dateRange.split(':')
-        const newDateRange = dr
-        const YEAR = 365 * 24 * 60 * 60 * 1000
-        const DATE_RANGE = new Date(dr[1]).getTime() - new Date(dr[0]).getTime()
-        const year = parseInt(DATE_RANGE / YEAR)
+    _handleDateRange(dateRange) {
+      const index = dateRange.indexOf(":");
+      if (index > -1) {
+        const dr = dateRange.split(":");
+        const newDateRange = dr;
+        const YEAR = 365 * 24 * 60 * 60 * 1000;
+        const DATE_RANGE =
+          new Date(dr[1]).getTime() - new Date(dr[0]).getTime();
+        const year = parseInt(DATE_RANGE / YEAR);
         if (year > 0) {
           for (let i = 0; i < year; i++) {
-            const date = this._getDate(newDateRange[i])
-            newDateRange.splice(i + 1, 0, date)
+            const date = this._getDate(newDateRange[i]);
+            newDateRange.splice(i + 1, 0, date);
           }
           for (let i = 0, length = newDateRange.length; i < length - 1; i++) {
-            newDateRange[i] = `${newDateRange[i]}:${newDateRange[i + 1]}`
+            newDateRange[i] = `${newDateRange[i]}:${newDateRange[i + 1]}`;
           }
-          newDateRange.length = year + 1
-          return newDateRange
+          newDateRange.length = year + 1;
+          return newDateRange;
         }
-        return dateRange
+        return dateRange;
       }
-      return dateRange
+      return dateRange;
     },
 
-    _getDate (date) {
-      const dateArr = date.split('-')
-      dateArr[0] = Number(dateArr[0]) + 1
-      return dateArr.join('-')
+    _getDate(date) {
+      const dateArr = date.split("-");
+      dateArr[0] = Number(dateArr[0]) + 1;
+      return dateArr.join("-");
     },
 
-    _handlePackages (packages) {
+    _handlePackages(packages) {
       if (Array.isArray(packages)) {
-        return `-,${packages.join(',')}`
+        return `-,${packages.join(",")}`;
       }
-      return packages
+      return packages;
     }
   }
-}
+};
 </script>
 
 
 <style lang="stylus" scoped>
 .home-page-one-wrapper {
   padding-top: 3.4rem;
+  background-image: url('./images/bg.jpg');
+  background-attachment: fixed;
 
   section {
     &.description {
@@ -171,7 +189,7 @@ export default {
       .btn-about {
         margin: 2rem 0;
         display: inline-block;
-        padding: .6rem 1.2rem;
+        padding: 0.6rem 1.2rem;
         border-radius: 0.25rem;
         background: $accentColor;
         color: #fff;
@@ -187,7 +205,7 @@ export default {
     &.wish {
       overflow: hidden;
 
-      &.yesterday, &.tomorrow {
+      &.area {
         background: var(--code-color);
       }
 
@@ -232,8 +250,9 @@ export default {
         h1 {
           margin-top: 5rem;
         }
+
         .btn-about {
-          font-size .9rem
+          font-size: 0.9rem;
         }
       }
 
@@ -264,8 +283,9 @@ export default {
         h1 {
           margin-top: 5rem;
         }
+
         .btn-about {
-          font-size .8rem
+          font-size: 0.8rem;
         }
       }
 
