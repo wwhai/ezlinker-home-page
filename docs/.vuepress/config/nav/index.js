f9f3f6839f920c9baf987f212c7bb53269b5c403
wwhai <751957846@qq.com>
2020-04-01 15:23:11 +0800

    删除emoj

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -5,22 +5,21 @@ module.exports = {
             items: [{
                     text: '发布版本',
                     items: [
-                        { text: '😍1.0', link: '/views/1.0/' }
+                        { text: 'V-1.0', link: '/views/1.0/' }
                     ],
                 },
                 {
                     text: '生态项目',
                     items: [
-                        { text: '❤️EZ-EMQX', link: '/views/ezemqx/' },
-                        { text: '🏧插件市场', link: '/views/pluginmarket/' },
-                        { text: '🔆开源硬件', link: '/views/openhardware/' }
+                        { text: 'EZ-EMQX', link: '/views/ezemqx/' },
+                        { text: '插件市场', link: '/views/pluginmarket/' },
+                        { text: '开源硬件', link: '/views/openhardware/' }
 
                     ]
                 }
             ]
 
         },
-        // { text: '文档', link: '/views/1.0/index', icon: 'reco-api' },
         { text: '常见问题', link: '/views/question/index.html', icon: 'reco-faq' },
         { text: '案例', link: '/views/example/index.html', icon: 'reco-category' },
         { text: '博客', link: 'https://space.ezlinker.cn', icon: 'reco-blog' },
